2dd349ca61ba7be8eb09a122e4504534518ab0dd
jahnavisen <jahnavi.sen02@gmail.com>
2018-08-10 17:36:12 +0000

    Done.

--- detention.rb
+++ detention.rb
@@ -1,1 +1,47 @@
 #Code your detention class here
+class Detention 
+  def initialize
+    @activity="collective punishment"
+    @students=[]
+  end
+  def activity
+    @activity
+  end
+  def students
+    @students
+  end
+  def add_student(name)
+    @students.push(name)
+  end
+  
+  def remove_student(name)
+    x = @students.length
+    count = 0 
+    while count <= x
+      if @students[count] == name
+        break
+      end
+      count += 1
+    end
+    @students.delete_at(count)
+  end
+  
+  def location
+    @location
+  end
+  def location=(room)
+    @location=room
+  end 
+  def time 
+    @time
+  end
+  def time=(clock)
+    @time=clock
+  end
+  def grumpy_teacher
+    @grumpy_teacher
+  end
+  def grumpy_teacher=(punish)
+    @grumpy_teacher=punish
+  end
+end 
